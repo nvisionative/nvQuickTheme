1a50a63e4b3b480973358bae83d91fc6144969c6
john-Graham <johndgraham@mac.com>
2018-07-12 14:55:46 -0500

    Switched Compressed to compact for more reliable sourceMap

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('images', function() {
 gulp.task('scss', function() {
   return gulp.src( './'+src+'scss/**/*.scss')
     .pipe(sourcemaps.init({largeFile: true}))
-      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
       .pipe(rename({suffix: '.min'}))
       .pipe(autoprefixer({browsers: ['last 2 versions', 'ie >= 9', '> 1%']}))
     .pipe(sourcemaps.write('.'))
@@ -70,13 +70,13 @@ gulp.task('scss', function() {
 gulp.task('bscss', function() {
   return gulp.src('./'+src+assets+'bootstrap/scss/**/*.scss')
     .pipe(sourcemaps.init({largeFile: true}))
-      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
       .pipe(rename({suffix: '.min'}))
       .pipe(autoprefixer({browsers: ['last 2 versions', 'ie >= 9', '> 1%']}))
     .pipe(sourcemaps.write('.'))
       .pipe(gulp.dest( './'+dist+'/css/'))
       .pipe(notify({message: 'Styles compiled successfully!', title : 'bootstrap', sound: false}));
-})
+});
 
 
 
