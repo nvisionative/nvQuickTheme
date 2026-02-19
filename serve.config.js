1ed0432b43482ff6bcc1f105bd254743c12989ba
tksheppard <tksheppard@nvisionative.com>
2026-02-19 10:48:52 -0500

    Add browser-sync config

---
+++ serve.config.js
@@ -0,0 +1,7 @@
+export default {
+    dnnUrl: 'http://mysite.loc',
+    port: 3000,
+    watchPaths: ['dist/**/*', 'containers/**/*', '*.ascx'],
+    sourcePaths: ['src/**/*'],
+    debounce: 300,
+};
